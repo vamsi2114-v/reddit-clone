44821ae3e16ebcc1538cde7092e562013ea74af5
Vamsi <vamsikrishnan1008@gmail.com>
2026-05-22 13:40:25 +0530

    Force dynamic all API routes

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -53,3 +53,4 @@ export async function POST(req: Request) {
     return NextResponse.json({ error: 'Server error' }, { status: 500 })
   }
 }
+export const dynamic = 'force-dynamic' 
--- src/app/api/votes/route.ts
+++ src/app/api/votes/route.ts
@@ -43,3 +43,4 @@ export async function POST(req: Request) {
     return NextResponse.json({ error: 'Server error' }, { status: 500 })
   }
 }
+export const dynamic = 'force-dynamic' 
