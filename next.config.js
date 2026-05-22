67850d6da4a6654c5bf22d267d6bfdf1811e861c
Vamsi <vamsikrishnan1008@gmail.com>
2026-05-22 13:09:30 +0530

    Fix Prisma build issue

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'res.cloudinary.com' },
